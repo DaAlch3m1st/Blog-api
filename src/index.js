8700ea4155f75fa2d251f1446ee4c6feaa5fba91
Bryan <bryan.alessandromc.6061@gmail.com>
2025-02-03 21:15:53 -0500

    appending the routes and ejs

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'))
 app.use(express.static('styles'))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+// linking my routes
+const signupRouter = require('./routes/signup');
+const loginUser = require('./routes/login');
+
+app.use('/', signupRouter);
+app.use('/', loginUser);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
