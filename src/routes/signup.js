ee239e04fd55f52a4d0a18bfbacba6657064b20e
Bryan <bryan.alessandromc.6061@gmail.com>
2025-02-03 22:03:42 -0500

    basic signup

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,9 +1,23 @@
-const express = require("express");
+const express = require("express")
 const router = express.Router()
+const connection = require('../db')
 
 router.get("/signup", function (req, res) {
     console.log("running the signup route")
     res.render("signup")
 });
-  
+
+router.post("/signup", function (req, res) {
+    const username = req.body.signupUsername
+    const password = req.body.signupPassword
+    console.log(`The username of the client is: ${username}`)
+    console.log(`The password of the client is: ${password}`)
+
+    connection.query(`INSERT INTO users (username, user_password) VALUES ("${username}", "${password}")`, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log("Info sent to the database")
+    })
+})
+
 module.exports = router
